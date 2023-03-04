979471fbe17ec4af84e0cb2af7e56b90895ec027
Carlos Matheus <carlos.matheus@outlook.com>
2023-03-04 19:51:26 -0300

    Program.cs atualizado
    - implementado menu para escolher unidade.
    - implementada chamadas aos métodos da classe Temperaturas.

--- Models/Temperaturas.cs
+++ Models/Temperaturas.cs
@@ -1,26 +1,27 @@
-namespace ConversorTemperaturas;
-
-public class Temperaturas
+namespace ConversorTemperaturas.Models
 {
-    public double celsius { get; set; }
-    public double fahrenheit { get; set; }
-    public double kelvin { get; set; }
-
-    public void ConverterCelsius(double temperatura)
+    public class Temperaturas
     {
-        fahrenheit = (temperatura * 1.8000) + 32.00;
-        kelvin = (temperatura * 1.000000) + 273.15;
-    }
+        public double celsius = 0.0;
+        public double fahrenheit = 0.0;
+        public double kelvin = 0.0;
 
-    public void ConverterFahrenheit(double temperatura)
-    {
-        celsius = (temperatura - 32) / 1.8000;
-        kelvin = ((temperatura - 32) / 1.8000) + 273.15;
-    }
+        public void ConverterCelsius(double temperatura)
+        {
+            fahrenheit = (temperatura * 1.8000) + 32.00;
+            kelvin = (temperatura * 1.000000) + 273.15;
+        }
 
-    public void ConverterKelvin(double temperatura)
-    {
-        celsius = temperatura - 273.15;
-        fahrenheit = ((temperatura - 273.15) * 1.8000) + 273.15;
+        public void ConverterFahrenheit(double temperatura)
+        {
+            celsius = (temperatura - 32) / 1.8000;
+            kelvin = ((temperatura - 32) / 1.8000) + 273.15;
+        }
+
+        public void ConverterKelvin(double temperatura)
+        {
+            celsius = temperatura - 273.15;
+            fahrenheit = ((temperatura - 273.15) * 1.8000) + 273.15;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConversorTemperaturas.Models;
+
+public class Program
+{
+    static void Main()
+    {
+        bool finalizaApp = false;
+        double valorTemp = 0.0;
+        Temperaturas temperatura = new Temperaturas();
+
+        Console.WriteLine("Conversor de Temperaturas\r");
+        Console.WriteLine("-------------------------\n");
+
+        while (!finalizaApp)
+        {
+            Console.WriteLine("Selecione abaixo a unidade de temperatura que deseja converter:");
+            Console.WriteLine("[c] - Celsius");
+            Console.WriteLine("[f] - Fahrenheit");
+            Console.WriteLine("[k] - Kelvin");
+            Console.Write("Sua opção?: ");
+
+            string opcao = Console.ReadLine();
+
+            Console.Write("Insira o valor da temperatura escolhida: ");
+            valorTemp = Convert.ToDouble(Console.ReadLine());
+
+            switch (opcao)
+            {
+                case "c":
+                    temperatura.ConverterCelsius(valorTemp);
+                    break;
+                case "f":
+                    temperatura.ConverterFahrenheit(valorTemp);
+                    break;
+                case "k":
+                    temperatura.ConverterKelvin(valorTemp);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida! Tente novamente...");
+                    break;
+            }
+        }
+    }
+}
